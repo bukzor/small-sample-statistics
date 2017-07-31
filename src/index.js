d09b6bc3a91ba1dae9a28e9845bf594344338790
Buck Evan <buck.2019@gmail.com>
2017-07-31 13:02:13 -0700

    show the samples, along with count, mean and variance

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 const XML = "http://www.w3.org/2000/svg";
+const samples = [];
 
+    console.log(1);
 let init = function() {
-  // <circle cx="100" cy="100" r="100"/>
-  document.querySelectorAll('.samples').forEach(samples => {
-    samples.addEventListener('click', event => {
+    console.log(2);
+  document.querySelectorAll('.sampleInput').forEach(sampleInput => {
+    console.log(3);
+    sampleInput.addEventListener('click', event => {
+    console.log(4);
+      // <circle cx="100" cy="100" r="100"/>
       let point = document.createElementNS(XML, 'circle');
       point.setAttribute('cx', event.offsetX);
       point.setAttribute('cy', event.offsetY);
       point.setAttribute('r', 3);
+      sampleInput.append(point);
 
-      samples.append(point);
+      samples.push(event.offsetX);
+      samples.sort();
+
+      renderSamples();
+      renderStatistics();
     });
   });
-}
+};
+
+let renderSamples = function() {
+  document.querySelector('.samples').innerText = samples.join(', ');
+};
+
+let renderStatistics = function() {
+  let count = samples.length;
+  document.querySelector('.statistics__count').innerText = count;
+  let mean = (
+    samples.reduce((acc, next) => acc + next, 0) / count
+  );
+  document.querySelector('.statistics__mean').innerText = mean.toFixed(2);
+  document.querySelector('.statistics__variance').innerText = Math.pow(
+    samples.reduce((acc, next) => acc + Math.pow(next - mean, 2), 0) / count,
+    0.5,
+  ).toFixed(2);
+};
 
 document.addEventListener("DOMContentLoaded", init);
