126feced698d4f1036d28c75c610dd465ba7e3ae
Buck Evan <buck.2019@gmail.com>
2017-08-02 15:55:27 -0700

    tolerance configuration

--- src/index.js
+++ src/index.js
@@ -1,44 +1,91 @@
-const XML = "http://www.w3.org/2000/svg";
-const samples = [];
+const XML = "http://www.w3.org/2000/svg"
+const data = {
+  samples: [],
+  min: NaN,
+  max: NaN,
+  percent: NaN,
+}
+const toleranceInput = "toleranceInput"
 
-    console.log(1);
-let init = function() {
-    console.log(2);
-  document.querySelectorAll('.sampleInput').forEach(sampleInput => {
-    console.log(3);
-    sampleInput.addEventListener('click', event => {
-    console.log(4);
-      // <circle cx="100" cy="100" r="100"/>
-      let point = document.createElementNS(XML, 'circle');
-      point.setAttribute('cx', event.offsetX);
-      point.setAttribute('cy', event.offsetY);
-      point.setAttribute('r', 3);
-      sampleInput.append(point);
+// let's use blissful?
+let $ = document.querySelector.bind(document)
+let $$ = document.querySelectorAll.bind(document)
 
-      samples.push(event.offsetX);
-      samples.sort();
-
-      renderSamples();
-      renderStatistics();
-    });
-  });
-};
 
 let renderSamples = function() {
-  document.querySelector('.samples').innerText = samples.join(', ');
-};
+  $('.samples').innerText = data.samples.join(', ')
+}
+
 
 let renderStatistics = function() {
-  let count = samples.length;
-  document.querySelector('.statistics__count').innerText = count;
+  let count = data.samples.length
+  $('.statistics__count').innerText = count
   let mean = (
-    samples.reduce((acc, next) => acc + next, 0) / count
-  );
-  document.querySelector('.statistics__mean').innerText = mean.toFixed(2);
-  document.querySelector('.statistics__variance').innerText = Math.pow(
-    samples.reduce((acc, next) => acc + Math.pow(next - mean, 2), 0) / count,
+    data.samples.reduce((acc, next) => acc + next, 0) / count
+  )
+  $('.statistics__mean').innerText = mean.toFixed(2)
+  $('.statistics__variance').innerText = Math.pow(
+    data.samples.reduce((acc, next) => acc + Math.pow(next - mean, 2), 0) / count,
     0.5,
-  ).toFixed(2);
-};
+  ).toFixed(2)
+}
 
-document.addEventListener("DOMContentLoaded", init);
+
+let render = function() {
+  renderSamples()
+  renderStatistics()
+}
+
+
+let sampleInputClick = function(event) {
+  // <circle cx="100" cy="100" r="100"/>
+  let point = document.createElementNS(XML, 'circle')
+  point.setAttribute('cx', event.offsetX)
+  point.setAttribute('cy', event.offsetY)
+  point.setAttribute('r', 3)
+  event.currentTarget.append(point)
+
+  data.samples.push(event.offsetX)
+  data.samples.sort()
+
+  render()
+}
+
+let initToleranceInput = function() {
+  let svg = $('svg')
+  let min = svg.x.baseVal.value
+  let max = min + svg.width.baseVal.value
+  for (slider of ["min", "max"]) {
+    slider = $('.' + toleranceInput + '__' + slider)
+    slider.setAttribute('min', min)
+    slider.setAttribute('max', max)
+    changeToleranceInput({target:slider})
+  }
+  changeToleranceInput({target:$('.' + toleranceInput + '__percent')})
+}
+
+let changeToleranceInput = function(event) {
+  let slider = event.target
+  let value = slider.value
+  let name = Array.from(slider.classList).filter(
+    className => className.startsWith(toleranceInput + '__')
+  )[0].slice(toleranceInput.length + 2)
+
+  data[name] = Number(value);
+  slider
+    .parentElement
+    .querySelector('output')
+    .innerText = value
+}
+
+
+let init = function() {
+  document
+    .querySelector('.sampleInput')
+    .addEventListener('click', sampleInputClick)
+
+  initToleranceInput()
+  document
+    .querySelectorAll('[class^="' + toleranceInput + '__"]')
+    .forEach(e => e.addEventListener('change', changeToleranceInput))
+}
