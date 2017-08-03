af61ebc3d2211838d6f1996394a2b8e246c21832
Buck Evan <buck.2019@gmail.com>
2017-08-03 11:19:26 -0700

    represent tolerated normal distributions visually

--- src/index.js
+++ src/index.js
@@ -11,6 +11,78 @@ let $ = document.querySelector.bind(document)
 let $$ = document.querySelectorAll.bind(document)
 
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+function svgNode(n, v) {
+  n = document.createElementNS("http://www.w3.org/2000/svg", n);
+  for (var p in v) n.setAttribute(p, v[p]);
+  return n
+}
+
+function colorizeThreshold(value, threshold) {
+  let percentage = value / threshold * 100
+  if ( percentage >= 100 ) {
+    S = 100;
+    L = 55;
+  } else {
+    S = 80
+    L = 45;
+  }
+  return 'hsl(' + value + ', ' + S + '%, ' + L + '%)'
+}
+
+let normalCDF = function(x, mean, variance) {
+  return 1/2 * (1 + math.erf(
+    (x - mean) / Math.sqrt(2 * variance)
+  ))
+}
+
+let renderToleranceArea = function() {
+  let ta = $('.toleranceArea')
+  let minX = ta.x.baseVal.value
+  let maxX = minX + ta.width.baseVal.value
+  let minY = ta.y.baseVal.value
+  let maxY = minY + ta.height.baseVal.value
+  let step = 3;
+
+  ta.innerHTML = '';
+  for (x = minX; x <= maxX; x = x + step) {
+    let mean = x;
+    for (y = minY; y <= maxY; y = y + step) {
+      let variance = Math.pow(y, 2)
+      let value = 100 * (
+        normalCDF(data.max, mean, variance) -
+        normalCDF(data.min, mean, variance)
+      )
+      ta.append(svgNode('circle', {
+        cx: x,
+        cy: y,
+        r: 2,
+        "data-value": value,
+        fill: colorizeThreshold(value, data.percent),
+      }))
+    }
+  }
+}
+
+
 let renderSamples = function() {
   $('.samples').innerText = data.samples.join(', ')
 }
@@ -29,20 +101,22 @@ let renderStatistics = function() {
   ).toFixed(2)
 }
 
+debouncedRenderToleranceArea = debounce(renderToleranceArea, 200)
 
 let render = function() {
   renderSamples()
   renderStatistics()
+  debouncedRenderToleranceArea();
 }
 
 
 let sampleInputClick = function(event) {
   // <circle cx="100" cy="100" r="100"/>
-  let point = document.createElementNS(XML, 'circle')
-  point.setAttribute('cx', event.offsetX)
-  point.setAttribute('cy', event.offsetY)
-  point.setAttribute('r', 3)
-  event.currentTarget.append(point)
+  event.currentTarget.append(svgNode('circle', {
+    cx: event.offsetX,
+    cy: event.offsetY,
+    r: 3,
+  }))
 
   data.samples.push(event.offsetX)
   data.samples.sort()
@@ -51,9 +125,11 @@ let sampleInputClick = function(event) {
 }
 
 let initToleranceInput = function() {
-  let svg = $('svg')
+  let svg = $('.sampleInput')
   let min = svg.x.baseVal.value
   let max = min + svg.width.baseVal.value
+  console.log('min: ' + min)
+  console.log('max:', max)
   $$('.toleranceInput__slider').forEach(slider => {
     if (['min', 'max'].includes(slider.name)) {
       slider.setAttribute('min', min)
@@ -86,6 +162,8 @@ let inputToleranceInput = function(event) {
   if (! isNaN(data.percent)) {
     rect.style.fillOpacity = data.percent / 100;
   }
+
+  render();
 }
 
 
@@ -98,4 +176,14 @@ let init = function() {
   document
     .querySelectorAll('.toleranceInput__slider')
     .forEach(e => e.addEventListener('input', inputToleranceInput))
+
+  renderToleranceArea();
+}
+
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", init);
+}
+else {
+  init();
 }
